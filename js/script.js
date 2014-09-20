79abd5d75670833b37c5af3c8bbc782ce0c8259b
Roy van de Water <dev+roy@integrumtech.com>
2014-09-20 12:10:21 -0700

    Disable 'niceScroll'

--- js/script.js
+++ js/script.js
@@ -7,19 +7,6 @@ $(document).ready(function(){
   $('.promo-03').parallax();
   $('.promo-04').parallax();
 
-  /* nice scroll */
-  $( 'html' ).niceScroll({
-    cursorcolor: '#5BBF21',
-    cursorwidth: '10px',
-    cursorborder: '1px solid #5BBF21',
-    cursoropacitymax: 0.9,                
-    scrollspeed: 120,
-    autohidemode: false,
-    horizrailenabled: false,
-    cursorborderradius: 2,
-    zindex: 1060
-  });
-
   /* scrolltop */
   $('.navbar-nav li a, .menus').on('click', function(event) {
     var $anchor = $(this);
@@ -38,25 +25,4 @@ $(document).ready(function(){
 	$('.navbar-collapse ul li a').click(function() {
 	    $('.navbar-toggle:visible').click();
 	});
-
-	/* map contact */
-  $("#map").gmap3({
-    map: {
-        options: {
-          center: [33.426369,-111.939745], /* change langitude latitude of your location here */
-          zoom: 12,
-          scrollwheel: false
-        }  
-     },
-    marker:{
-        latLng: [33.426369,-111.939745], /* change langitude latitude of your location here */
-        options: {
-         icon: new google.maps.MarkerImage(
-           "https://dl.dropboxusercontent.com/u/29545616/Preview/location.png",
-           new google.maps.Size(48, 48, "px", "px")
-         )
-        }
-     }
-  });
-
 });
